3c676fed2f2e4d452ee04612018e9cbd428144db
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-08 19:44:44 +0100

    a

---
+++ src/components/Project.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css';
+
+
+const Project = () => {
+    return (
+        <div>
+            <h1>Project</h1>
+        </div>
+    );
+};
+
+export default Project;
