18d9833c9b162d5b6b3fa855e4541d86f732da4b
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-08 20:19:53 +0100

    Début portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,14 @@ import '../App.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Bonjour</h1>
-        </div>
+    <header>
+        <a href="#home" className="logo">Jul<span>ien</span></a>
+        <ul className="navigation">
+            <li><a href="#Project">Project</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Contact">Contact</a></li>
+        </ul> 
+   </header>
     );
 };
 
