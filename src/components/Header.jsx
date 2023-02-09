df5aaf758e4436d4921153f0f40c1dbeee0b313d
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-09 23:17:06 +0100

    start header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import '../App.css';
 
+const julien = require('../img/julien.png')
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className="all">
+            <div className="title-box">
+                <span className="wave">👋</span>
+                <p className="P-text">Hello, i'm</p>
+                <p className="head-text">Julien</p>
+            </div>
+            <div className="work-box">
+                <p className="work">Full-stack Developper</p>
+                <p className="work">Junior</p>
+            </div>
+            <div className="img-box">
+                <img src={julien} alt="img de moi" className="imgJ"/>
+            </div>
         </div>
     );
 };
