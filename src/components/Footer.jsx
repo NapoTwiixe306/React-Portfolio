afc3731e719feb1800030b76ef77c85595e4497c
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-10 16:18:19 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import '../App.css';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
-    return (
-        <div className="tqt">
-            <h1>Footer</h1>
-        </div>
-    );
+  return (
+    <div className="footer">
+      <p className='footer_text'>Copyright © 2023 Julien-Milants   |</p>
+      <a href='https://github.com/NapoTwiixe306' target="_blank" rel='noopener noreferrer' className='lofo'><FontAwesomeIcon icon={faGithub} size="2x" className='github'/></a>
+      <a href='https://CryptoExtension.fr' target="_blank" rel='noopener noreferrer' className='lofo'><FontAwesomeIcon icon={faGoogle} size="2x" className='github'/></a>
+      <a href='https://www.instagram.com/julien.mlnts/' target="_blank" rel='noopener noreferrer' className='lofo'><FontAwesomeIcon icon={faInstagram} size="2x" className='github'/></a>
+  </div>
+  );
 };
 
 export default Footer;
