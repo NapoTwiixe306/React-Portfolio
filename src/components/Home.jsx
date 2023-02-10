0be38b0868bcc88740aee9dfe1fd0b9f7b51b252
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-10 18:20:38 +0100

    contact button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <li><a href="#Skill">Skill</a></li>
             <li><a href="#Project">Project</a></li>
             <li><a href="#About">About</a></li>
-            <li><a href="#Contact">Contact</a></li>
+            <li><a href="#Contact" className="action">Contact</a></li>
         </ul> 
    </header>
     );
