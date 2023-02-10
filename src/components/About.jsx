282b56259ec40ca81c5fd7c9b049d7486a4d7339
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-10 16:03:32 +0100

    about.jsx finish

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,10 @@ import '../App.css';
 
 const About = () => {
     return (
-        <div className="tqt" id="About">
-            <h1>About</h1>
+        <div id="About" className="about">
+            <h1 className="about-title">About Me</h1>
+            <p className="about-text">Hello, my name is Julien Milants and I am a full stack developer specialized in web development and web3.0. I’m 20 years old and I’m self-taught. From a young age, I’ve been fascinated by the technology and the opportunities it offers. So I decided to start learning web development, and I quickly understood that it was my passion. I spent all my time and energy learning different languages and technologies, and became an expert in full stack web development. In addition to my passion for web development, I am also interested in web3.0 and the new technologies that come with it. I am convinced that these technologies will revolutionize the way we work and communicate, and I am excited to be part of this revolution.</p>
+            <p className="about-text1">As a self-taught, I am always looking to learn new things and develop as a developer. I am also passionate about solving complex problems and I love working on ambitious projects. I am currently developing my portfolio with React.js, in order to present my skills and achievements to potential employers or clients. If you are looking for a talented and passionate web developer, I would be happy to discuss with you your projects and how I can help you. Copyright: This presentation was written by Julien Milants. Any unauthorized use of this text will be considered a violation of my copyright.</p>
         </div>
     );
 };
