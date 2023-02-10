139469e5fdf034018722fb0566c4604d77c3d5bc
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-10 13:53:39 +0100

    modifs home et css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
     <header>
         <a href="#home" className="logo">Julien</a>
         <ul className="navigation">
+            <li><a href="#Skill">Skill</a></li>
             <li><a href="#Project">Project</a></li>
             <li><a href="#About">About</a></li>
             <li><a href="#Contact">Contact</a></li>
