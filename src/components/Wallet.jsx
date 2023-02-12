bd336c91335de4a58417e80af1fa0e83b390bd23
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-02-12 20:37:15 +0100

    "find" de wallet (a paufiner) et modifs projetCard

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import chip from '../img/chip.png'
+import contactless from '../img/wifi-signal.png'
+import visa from '../img/visa.png'
+
 import '../App.css';
 
 const Wallet = () => {
@@ -43,9 +47,26 @@ const Wallet = () => {
 
     return (
         <div className="wallet" id="Wallet">
-            <h1>Wallet</h1>
+            <h1 className="wallet-title">Wallet</h1>
+
             <button id="loginButton" className="button-connection">{loginButtonText}</button>
-            <p id="userWallet" className="text-lg text-gray-600 my-2 ethAdresse">{userWalletAddress}</p>
+            <p id="userWallet" className="text-lg text-gray-600 my-2 ethAdresse"></p>
+        
+            <div class="card-container">
+                <div class="card1">
+                    <img class="chip" src={chip}alt=""/>
+                    <img class="contactless" src={contactless} alt=""/>
+                    <img class="visa" src={visa} alt="" />
+                    <p id="test" class="card-number">{userWalletAddress}</p>
+                    <div class="arrow">
+                    </div>
+                    <p class="card-name">Ethereum</p>
+                    <p class="bank-name">CryptoBank</p>
+                </div>
+            </div>
+
+            <p className="card-text">
+            the button below, allows ed you connect to your allet metamask, there will soon be the possibility to choose any wallet        </p>
         </div>
     );
 };
